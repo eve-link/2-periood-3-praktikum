3cf619853708996e5a2a0c597a4f81f0f57d55b2
eve-link <eve.link.1992@gmail.com>
2014-10-16 11:39:32 +0300

    form_processing.php
    mingi viga sees

--- form_processing.php
+++ form_processing.php
@@ -1,10 +1,40 @@
-<!DOCTYPE html>
+<DOCTYPE! HTML>
 <html>
-<head>
-<title>2-periood–3-praktikum</title>
-</head>
-<body>
-safsdf
+  <head>
+    <title>2-periood-3-praktikum</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type"text/css" href="style.css">
+  </head>
+  <body>
+  <?php 
+      $username = $_POST["username"];
+      $password = $_POST["password"];
+    ?>
 
-</body>
+    <pre>
+    <?php 
+    print_r($_POST);
+    ?>
+
+<?php
+  if (isset($_POST["username"])) {
+    $username = $_POST["username"];
+  } else {
+    $username = "puudub";
+  }
+?>
+
+<?php 
+    echo "Tere {$username}! Teie parool on {$password}.";
+    ?>
+    <br><br>
+
+<?php 
+if 
+  ($password = $_POST["password"])
+  {echo "Tere {$username}! Teie parool on {$password}";} 
+else 
+  {echo "Kasutajanimi või parool on puudu.";}
+?>
+  </body>
 </html>
